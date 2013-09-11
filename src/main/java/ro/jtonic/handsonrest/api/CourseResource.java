5f87b3fb2875cafc1c14287a626067b0867c23f2
jtonic <antonel.pazargic@gmail.com>
2013-09-11 19:56:14 +0300

    Add examples with UriInfo, MatrixParam, array/list query param and PathElement.

--- src/main/java/ro/jtonic/handsonrest/api/CourseResource.java
+++ src/main/java/ro/jtonic/handsonrest/api/CourseResource.java
@@ -1,8 +1,14 @@
 package ro.jtonic.handsonrest.api;
 
 import com.google.common.base.Objects;
+import com.google.common.collect.Iterables;
 
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.UriInfo;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,9 +16,8 @@ import java.util.List;
  */
 public class CourseResource implements ICourseResource {
     @Override
-    public String getCourses(List<Integer> ids) {
-        System.out.println("topics no = " + ids.size());
-        return Integer.toString(ids.size());
+    public String getCourses(String[] courseNames) {
+        return "Courses " + Arrays.deepToString(courseNames);
     }
 
     @Override
@@ -27,5 +32,30 @@ public class CourseResource implements ICourseResource {
         final String color = model.getMatrixParameters().getFirst("color");
         final String str = Objects.toStringHelper(this).add("model", mod).add("year", year).add("color", color).toString();
         return str;
+    }
+
+    @Override
+    public String getCourse(String course, String teacher, String name, String duration) {
+        final String ret = String.format("Course= %s, name = %s, duration = %s, teacher = %s", course, name, duration, teacher);
+        return ret;
+    }
+
+    @Override
+    public String getByCriteria(UriInfo uriInfo) {
+        final MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+        for (String name : queryParameters.keySet()) {
+            System.out.println("name = " + name);
+            System.out.println("value = " + queryParameters.getFirst(name));
+        }
+        final List<PathSegment> pathSegments = uriInfo.getPathSegments();
+        for (PathSegment pathSegment : pathSegments) {
+            System.out.println("pathSegment.getPath() = " + pathSegment.getPath());
+            final MultivaluedMap<String, String> matrixParameters = pathSegment.getMatrixParameters();
+            for (String matrixName : matrixParameters.keySet()) {
+                System.out.println("matrixName = " + matrixName);
+                System.out.println("matrixParameters.getFirst(matrixName) = " + matrixParameters.getFirst(matrixName));
+            }
+        }
+        return "The uriInfo info is logged on server.";
     }
 }
--- src/main/java/ro/jtonic/handsonrest/api/ICourseResource.java
+++ src/main/java/ro/jtonic/handsonrest/api/ICourseResource.java
@@ -1,10 +1,15 @@
 package ro.jtonic.handsonrest.api;
 
+import org.springframework.scheduling.quartz.SimpleTriggerBean;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
 /**
@@ -14,7 +19,7 @@ import java.util.List;
 public interface ICourseResource {
 
     @GET
-    String getCourses(@QueryParam("id") List<Integer> ids);
+    String getCourses(@QueryParam("name") String[] courseNames);
 
     @GET()
     @Path("query")
@@ -23,4 +28,15 @@ public interface ICourseResource {
     @GET()
     @Path("matrix/{model}")
     String getCoursesThroughMatrix(@PathParam("model") PathSegment pathSegment);
+
+    @GET()
+    @Path("{course}/{teacher}")
+    String getCourse(@PathParam("course") String course,
+                     @PathParam("teacher") String teacher,
+                     @MatrixParam("name") String name,
+                     @MatrixParam("duration") String duration);
+
+    @GET
+    @Path("criteria")
+    String getByCriteria(@Context UriInfo uriInfo);
 }
